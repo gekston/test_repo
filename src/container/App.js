39852b5bf0efda432b0eabb55595d771253244e9
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-04-16 20:52:19 +0300

    import data/ imglist component

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../assets/App.css';
+import SliderImage from '../components/SliderImage'
+import images from '../fixture'
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <SliderImage imgCollection={images} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
