7cb1a31e6b520a296c9a26f81fc198d53adaf43e
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-04-18 11:57:31 +0300

    2 cpaces

--- src/components/SliderImage/index.js
+++ src/components/SliderImage/index.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types'
 import './style.css'
 
 class SliderImg extends Component {
-    state = {
-        sliderCount: 1
-    }
+  state = {
+      sliderCount: 1
+  }
 
-    render() {
-        const { imgCollection } = this.props
-        const ArrayOfImg = imgCollection[0].imgCollection
-        console.log(imgCollection[0].imgCollection)
-        const imgList = ArrayOfImg.map((index) =>
-                <div className="wrapper_item" key={(index.id).toString()}>
-                    <img className="wrapper_item-img" src={index.src} alt={index.id} />
-                </div>
-            )
-        return (
-            <div className="list_wrapper">{imgList}</div>
-        )
-    }
+  render() {
+      const { imgCollection } = this.props
+      const ArrayOfImg = imgCollection[0].imgCollection
+      console.log(imgCollection[0].imgCollection)
+      const imgList = ArrayOfImg.map((index) =>
+              <div className="wrapper_item" key={(index.id).toString()}>
+                  <img className="wrapper_item-img" src={index.src} alt={index.id} />
+              </div>
+          )
+      return (
+          <div className="list_wrapper">{imgList}</div>
+      )
+  }
 }
 
 SliderImg.propTypes = {
-    imgCollection: PropTypes.array.isRequired
+  imgCollection: PropTypes.array.isRequired
 }
 
 export default SliderImg
